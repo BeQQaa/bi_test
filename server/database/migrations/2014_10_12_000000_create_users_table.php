a9540119ecb59abc98bc5f47dd65a1b79bc0fe94
Oleh <beqqaa@MacBook-Air-Oleh.local>
2023-10-14 06:05:21 +0200

    add migration

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,15 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number')->nullable();
+            $table->unsignedBigInteger('company_id');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('company_id')->references('id')->on('company');
         });
+
     }
 
     /**
