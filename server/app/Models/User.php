4c0648d64ee453078b076b0f461c8cb24cedbf1d
Oleh <beqqaa@Air-Oleh.home>
2023-10-15 13:42:33 +0200

    update user_model

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Exceptions\WrongPasswordException;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Http\Response;
@@ -29,7 +30,8 @@ class User extends Authenticatable
         'email',
         'password',
         'company_id',
-        'phone_number'
+        'phone_number',
+        'role'
     ];
 
     /**
@@ -60,13 +62,27 @@ class User extends Authenticatable
      */
     public final static function attempt(string $password, string $storedPassword): void
     {
-        if(!Hash::check($password, $storedPassword)){
+        if (!Hash::check($password, $storedPassword)) {
             throw new WrongPasswordException('Invalid password', Response::HTTP_LOCKED);
         }
+    }
+
+    /**
+     * @param string $validated
+     * @param Model|User $user
+     * @return string
+     */
+    public static function roleType(string $validated, Model|User $user): string
+    {
+        return $validated == config('constants.ADMIN_NAME') ?
+            $user->createToken(config('constants.USER_TOKEN_ADMIN'), config('constants.ADMIN_ABILITIES'))->plainTextToken :
+            $user->createToken(config('constants.USER_TOKEN_USER'), config('constants.USER_ABILITIES'))->plainTextToken;
     }
 
     public final function company(): Company
     {
         return $this->company->belongsTo(Company::class);
     }
+
+
 }
