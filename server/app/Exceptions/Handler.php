f55abb1c13f18f447d1c5c15d5b1218218f90b3a
Oleh <beqqaa@Air-Oleh.home>
2023-10-15 13:40:40 +0200

    handle exception on GATE

--- server/app/Exceptions/Handler.php
+++ server/app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -21,10 +26,19 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
+    public final function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public final function render($request, Exception|Throwable $e): Response|JsonResponse|RedirectResponse|\Symfony\Component\HttpFoundation\Response
+    {
+        if ($e instanceof AuthorizationException) {
+            return new JsonResponse(config('responses.ACCESS_DENY'));
+        }
+
+        return parent::render($request, $e);
     }
 }
