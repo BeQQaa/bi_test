6a6421369fe30484bb445285786ba87639a5b372
Oleh <beqqaa@MacBook-Air-Oleh.local>
2023-10-17 20:38:26 +0200

    add tests

---
+++ server/tests/Feature/RegistrationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Faker\Factory as Faker;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * @test
+     */
+    public function user_can_register(): void
+    {
+        $faker = Faker::create();
+
+
+
+        $response = $this->putJson('api/auth/', [
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => $faker->unique()->email,
+            'password' => $faker->password,
+            'role' => 'admin',
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJsonStructure(['token']);
+    }
+}
